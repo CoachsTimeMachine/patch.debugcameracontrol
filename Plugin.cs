cc4021e787cee848f8fe525a7aa1cbf43024ed43
stuartt <34777184+RealMCoded@users.noreply.github.com>
2026-04-25 18:35:34 -0400

    Initial Commit

---
+++ Plugin.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.VR;
+
+[BepInPlugin("com.ctm.patchdebugcameracontrol", "patch.DebugCameraControl", "1.0.0")]
+public class VRDebugModPlugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        Logger.LogInfo("DebugCameraControl patch - https://github.com/CoachsTimeMachine/patch.debugcameracontrol");
+        var harmony = new Harmony("com.ctm.patchdebugcameracontrol");
+        harmony.PatchAll();
+        Logger.LogInfo("Patched all methods!");
+    }
+}
+
+//Always return true on VRDevice.isPresent to make the game think a VR headset is always connected.
+[HarmonyPatch(typeof(VRDevice), nameof(VRDevice.isPresent), MethodType.Getter)]
+public class Patch_VRDevice_isPresent
+{
+    static bool Prefix(ref bool __result)
+    {
+        __result = true;
+        return false;
+    }
+}
+
+//Always return debug input from ControllerIO.Update by calling LMAGOHCBOGK() directly and skipping the original Update logic.
+[HarmonyPatch(typeof(ControllerIO), "Update")]
+public class Patch_ControllerIO_Update
+{
+    static bool Prefix(ControllerIO __instance)
+    {
+        AccessTools.Method(typeof(ControllerIO), "LMAGOHCBOGK")
+                    .Invoke(__instance, null);
+        return false;
+    }
+}
+
+// Force set DebugCameraControl.Awake flag to be true to prevent it from destroying itself (enabling DebugCameraControl).
+[HarmonyPatch(typeof(DebugCameraControl), "Awake")]
+public class Patch_DebugCameraControl_Awake
+{
+    static bool Prefix(DebugCameraControl __instance)
+    {
+        // Skip the original Awake (which would Destroy the component).
+        // Set IBFMMGKIJME = true so the free-cam starts enabled immediately.
+        AccessTools.Property(typeof(DebugCameraControl), "IBFMMGKIJME")
+                   .SetValue(__instance, true, null);
+        return false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+<div align="center">
+<img src="https://avatars.githubusercontent.com/u/274413706" width="128" height="128">
+<h1>Coach's Time Machine - DebugCameraControl [patch]</h1>
+
+[Please check this Readme for information about CTM, including a compatibility list.](https://github.com/CoachsTimeMachine/.github/blob/main/README.md)
+
+[Download precompiled binaries here](https://github.com/CoachsTimeMachine/patch.debugcameracontrol/releases)
+</div>
+
+---
+
+Small patch for early Rec Room builds to allow DebugCameraControl ("screen mode") inputs. Useful for trying private server updates without loading fully into VR. Currently uses obfuscated names, meaning this patch will only work on `20170209_EA` and newer.
+
+Tested on `20171002_TRAILER`, with BepInEx 5.4.23.5.
